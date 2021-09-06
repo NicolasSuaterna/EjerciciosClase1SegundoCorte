ba9d7e9cae4ff4d5d41543bf7757e08dfe756776
NicolasSuaterna <90203251+NicolasSuaterna@users.noreply.github.com>
2021-09-06 15:24:02 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,7 +9,10 @@ function start(){
 			nlat:30,
 			nlong:30,
 			radius:3,
-			//textures: 'https://lizgar.github.io/A3D/js/earth.jpg'
+			colors: [1.0, 0.0, 0.0, 1.0],
+			textures: 'https://lizgar.github.io/A3D/js/earth.jpg'
+			//textures: 'img/earth.jpg'
+			 
 				
 		}
 		
@@ -21,16 +24,18 @@ PhiloGL('glCanvas',
 	camera: {
 		position: {
 			x:0, y:0, z:-10
-			
-						
 		}
 	},
 	
 	
-	/*
+	
+	///////////////
+	
+	
 	textures:{
 		
 		src: ['https://lizgar.github.io/A3D/js/earth.jpg'],
+		//src: ['img/earth.jpg'],
 		parameters: [{
 			name: 'TEXTURE_MAG_FILTER',
 			value: 'LINEAR'
@@ -43,9 +48,12 @@ PhiloGL('glCanvas',
 			
 		]
 	},
-	*/
+	
+	
+		
 	
 ///////////////////
+
 
 events: {
       onDragStart: function(e) {
@@ -68,20 +76,96 @@ events: {
 		  
 		  
         e.stop();
+		
+		
         var camera = this.camera;
         camera.position.z += e.wheel;
         camera.update();
 		
 		
+      },
+	  
+	  //////////////////
+	  
+	  onKeyDown: function(e) {
+		  
+        switch(e.key) {
+         /* case 'f':
+            filter = (filter + 1) % 3;
+            break;*/
+          case 'w':
+            tierra.position.y += 0.1;
+			//console.log('lexiste');
+			tierra.update();
+            break;
+          case 's':
+            tierra.position.y -= 0.1;
+			tierra.update();
+            break;
+          case 'a':
+            tierra.position.x += 0.1;
+			tierra.update();
+            break;
+          case 'd':
+            tierra.position.x -= 0.1;
+			tierra.update();
+            break;
+		   
+		  ///////////////
+			
+			case 'u':
+            tierra.scale.y += 0.1;
+			console.log('lexiste');
+			tierra.update();
+            break;
+          case 'j':
+            tierra.scale.y -= 0.1;
+			tierra.update();
+            break;
+          case 'i':
+            tierra.scale.x += 0.1;
+			tierra.update();
+            break;
+          case 'k':
+            tierra.scale.x -= 0.1;
+			tierra.update();
+            break;
+			case 'o':
+            tierra.scale.z += 0.1;
+			tierra.update();
+            break;
+			case 'l':
+            tierra.scale.z -= 0.1;
+			tierra.update();
+            break;
+			
+          //handle page up/down
+          /*
+		  default:
+            if (e.code == 33) {
+              z -= 0.05;
+            } else if (e.code == 34) {
+              z += 0.05;
+            }
+			*/
+			
+        }
       }
+	  
+	   
+	  
+	  ////////////////////// 
+	  
+	  
     },
+		
+    			
 		
     onError: function() {
       alert("There was an error creating the app.");
     },
 
-///////////////////
-	
+
 	onLoad: function(app){
 		
 		var gl= app.gl,
@@ -89,9 +173,27 @@ events: {
 			scene = app.scene,
 			canvas = app.canvas,
 			camera = app.camera;
-///////////
 
-lighting = $id('lighting'),
+/*
+		lighting = $id('lighting'),
+          ambient = {
+            r: 0.5,
+            g: 0.4,
+            b: 0.4
+          },
+          direction = {
+            x: -1.0,
+            y: -1.0,
+            z: -1.0,
+          
+            r: 0.0,
+            g: 1.0,
+            b: 1.0
+          };
+*/
+
+
+		lighting = $id('lighting'),
           ambient = {
             r: $id('ambientR'),
             g: $id('ambientG'),
@@ -106,25 +208,16 @@ lighting = $id('lighting'),
             g: $id('directionalG'),
             b: $id('directionalB')
           };
-
-
-
-////////////////
+		  
+		  
 		
-		gl.clearColor(1.0, 1.0, 1.0, 1.0);
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.clearDepth(1.0);
 		gl.enable(gl.DEPTH_TEST);
 		gl.depthFunc(gl.LEQUAL);
 		gl.viewport(0,0, +canvas.width, +canvas.height);
 	
-			///////////////////
-			
-				 
-				//tierra.position.set(-1.5, 0, 0);
-				 
-				 
-			 //////////////////////////
-		
+					
 		tierra.update();
 		scene.add(tierra);
 		draw();
@@ -135,29 +228,53 @@ lighting = $id('lighting'),
 			
 			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPHT_BUFFER_BIT);
 			
-		///////////
-		/*
-		//Setup lighting
-        var lights = scene.config.lights;
+				/*
+		var lights = scene.config.lights;
         lights.enable = lighting.checked;
         lights.ambient = {
-          r: +ambient.r.value,
-          g: +ambient.g.value,
-          b: +ambient.b.value
+          r: +ambient.r,
+          g: +ambient.g,
+          b: +ambient.b
         };
         lights.directional = {
           color: {
-            r: +direction.r.value,
-            g: +direction.g.value,
-            b: +direction.b.value
+            r: +direction.r,
+            g: +direction.g,
+            b: +direction.b
           },
           direction: {
-            x: +direction.x.value,
-            y: +direction.y.value,
-            z: +direction.z.value
+            x: +direction.x,
+            y: +direction.y,
+            z: +direction.z
           }
         };
 		*/
+				
+		//Setup lighting
+		/*
+        var lights = scene.config.lights;
+        lights.enable = lighting.checked;
+        lights.ambient = {
+          r: 0.0,
+          g: 1.0,
+          b: 0.0
+        };
+        lights.directional = {
+          color: {
+            r: 1.0,
+            g: 1.0,
+            b: 0.0
+          },
+          direction: {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0
+          }
+        };
+		*/
+		
+		
+		
 		/////////////
 			
 			scene.render();
